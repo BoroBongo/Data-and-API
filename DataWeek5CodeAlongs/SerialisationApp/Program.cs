becaef03c4651ff21ee11a2e3f27bf141a1fcb6a
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2022-03-07 12:11:25 +0000

    Code Along created | created a Trainee class for later serialisation

---
+++ DataWeek5CodeAlongs/SerialisationApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SerialisationApp;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, world!");
+        Trainee trainee = new Trainee() { FirstName = "Adam", LastName = "Kolaczynski" };
+        Console.WriteLine(trainee);
+        Console.WriteLine(trainee.FirstName + " " + trainee.LastName);
+        Console.WriteLine(trainee.FullName);
+        Console.WriteLine(trainee.FullName);
+        Trainee trainee2 = new Trainee();
+        Console.WriteLine(trainee2.FullName);
+
+    }
+}
---
+++ DataWeek5CodeAlongs/SerialisationApp/Trainee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialisationApp
+{
+    public class Trainee
+    {
+        public string? FirstName { get; init; }
+        public string LastName { get; init; } = String.Empty;
+        public int SpartaNo { get; init; }
+        public string FullName => $"{this.FirstName} {this.LastName}";
+
+        public override string? ToString()
+        {
+            return $"{this.SpartaNo} - {this.FullName}";
+        }
+    }
+}
