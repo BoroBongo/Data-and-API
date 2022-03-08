d00c88cd0cb28b851ba6c5b125ad86c33ea7ecda
BoroBongo <43158992+BoroBongo@users.noreply.github.com>
2022-03-08 14:00:26 +0000

    halfway through the SQL exercises

--- CSharp_Database/SQLWithCSharp/Customer.cs
+++ CSharp_Database/SQLWithCSharp/Customer.cs
@@ -13,5 +13,6 @@ namespace SQLWithCSharp
         {
             return $"{ContactTitle} - {ContactName}";
         }
+
     }
 }
--- CSharp_Database/SQLWithCSharp/Program.cs
+++ CSharp_Database/SQLWithCSharp/Program.cs
@@ -19,79 +19,80 @@ namespace SQLWithCSharp
                 CompanyName = "ToysRUs"
             };
 
-            //using (var connection = new SqlConnection(@"Data Source= ????? "))
-            //{
-            //    //Testing our connection is open
-            //    connection.Open();
-            //    Console.WriteLine(connection.State);
+            using (var connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                //Testing our connection is open
+                connection.Open();
+                Console.WriteLine(connection.State);
 
-            //    using (var command = new SqlCommand("?????"))
-            //    {
-            //        //SQL reader Provides a way of reading a forward-only stream of rows from a SQL Server database. 
-            //        SqlDataReader sqlReader = command.ExecuteReader();
+                using (var command = new SqlCommand("select cs.CustomerID, cs.ContactName, cs.CompanyName, cs.City, cs.ContactTitle from Customers cs", connection))
+                {
+                    //command.Connection = connection;
+                    //SQL reader Provides a way of reading a forward-only stream of rows from a SQL Server database. 
+                    SqlDataReader sqlReader = command.ExecuteReader();
 
-            //        // loop while the reader has more data to read. The typical method of reading from the data stream returned by the SqlDataReader is to iterate through each row with a while loop
-            //        while (sqlReader.Read())
-            //        {
+                    // loop while the reader has more data to read. The typical method of reading from the data stream returned by the SqlDataReader is to iterate through each row with a while loop
+                    while (sqlReader.Read())
+                    {
 
-            //            //creating variables for customer
-            //            var customerID = sqlReader["CustomerID"].ToString();
-            //            var contactName = sqlReader["ContactName"].ToString();
-            //            var companyName = sqlReader["CompanyName"].ToString();
-            //            var city = sqlReader["City"].ToString();
-            //            var contactTitle = sqlReader["ContactTitle"].ToString();
+                        //creating variables for customer
+                        var customerID = sqlReader["CustomerID"].ToString();
+                        var contactName = sqlReader["ContactName"].ToString();
+                        var companyName = sqlReader["CompanyName"].ToString();
+                        var city = sqlReader["City"].ToString();
+                        var contactTitle = sqlReader["ContactTitle"].ToString();
 
-            //            //new customer object
-            //            var customer = new Customer() { ContactTitle = contactTitle, CustomerID = customerID, ContactName = contactName, City = city, CompanyName = companyName };
+                        //new customer object
+                        var customer = new Customer() { ContactTitle = contactTitle, CustomerID = customerID, ContactName = contactName, City = city, CompanyName = companyName };
 
-            //            customers.Add(customer);
-            //        }
+                        customers.Add(customer);
+                    }
 
-            //        // iterate over and output all customers
-            //        foreach (var c in customers)
-            //        {
-            //            Console.WriteLine($"Customer {c.GetFullName()} has ID {c.CustomerID} and lives in {c.City}");
-            //        }
-            //        // Close the reader
-            //        sqlReader.Close();
-            //    }
-            //    string sqlDeleteString = $"?????";
-            //    int affected = 0;
-            //    using (var command4 = new SqlCommand(sqlDeleteString, connection))
-            //    {
-            //        // if successful, this should equal 1
-            //        affected = command4.ExecuteNonQuery();
-            //    }
-            //    string sqlString = $"?????";
-            //    // execute insert SQL command
-            //    using (var command2 = new SqlCommand(sqlString, connection))
-            //    {
-            //        //Executes a Transact-SQL statement against the connection and returns the number of rows affected.
-            //        affected = command2.ExecuteNonQuery();
-            //    }
-            //    //NOTE: ExecuteNonQuery used for executing queries that does not return any data. It is used to execute the sql statements like update, insert, delete etc. ExecuteNonQuery executes the command and returns the number of rows affected.
+                    // iterate over and output all customers
+                    foreach (var c in customers)
+                    {
+                        Console.WriteLine($"Customer {c.GetFullName()} has ID {c.CustomerID} and lives in {c.City}");
+                    }
+                    // Close the reader
+                    sqlReader.Close();
+                }
+                string sqlDeleteString = $"DELETE from Customers where CustomerID = 'MANDA'";
+                int affected = 0;
+                using (var command4 = new SqlCommand(sqlDeleteString, connection))
+                {
+                    // if successful, this should equal 1
+                    affected = command4.ExecuteNonQuery();
+                }
+                string sqlString = $"INSERT INTO Customers(CustomerID, ContactName, City, CompanyName) VALUES ('{newCustomer.CustomerID}', '{newCustomer.ContactName}', '{newCustomer.City}','{newCustomer.CompanyName}')";
+                // execute insert SQL command
+                using (var command2 = new SqlCommand(sqlString, connection))
+                {
+                    //Executes a Transact-SQL statement against the connection and returns the number of rows affected.
+                    affected = command2.ExecuteNonQuery();
+                }
+                //NOTE: ExecuteNonQuery used for executing queries that does not return any data. It is used to execute the sql statements like update, insert, delete etc. ExecuteNonQuery executes the command and returns the number of rows affected.
 
-            //    string sqlUpdateString = $"?????";
+                //    string sqlUpdateString = $"?????";
 
-            //    using (var command3 = new SqlCommand(sqlUpdateString, connection))
-            //    {
-            //        affected = command3.ExecuteNonQuery();
-            //    }
-            //    using (var updateCustomerCommand = new SqlCommand("UpdateCustomer", connection))
-            //    {
-            //        // Using System.Data;
-            //        updateCustomerCommand.CommandType = CommandType.StoredProcedure;
-            //        // add parameters
-            //        updateCustomerCommand.Parameters.AddWithValue("???, ???");
-            //        updateCustomerCommand.Parameters.AddWithValue("???", "???");
-            //        // run the update
-            //        affected = updateCustomerCommand.ExecuteNonQuery();
+                //    using (var command3 = new SqlCommand(sqlUpdateString, connection))
+                //    {
+                //        affected = command3.ExecuteNonQuery();
+                //    }
+                //    using (var updateCustomerCommand = new SqlCommand("UpdateCustomer", connection))
+                //    {
+                //        // Using System.Data;
+                //        updateCustomerCommand.CommandType = CommandType.StoredProcedure;
+                //        // add parameters
+                //        updateCustomerCommand.Parameters.AddWithValue("???, ???");
+                //        updateCustomerCommand.Parameters.AddWithValue("???", "???");
+                //        // run the update
+                //        affected = updateCustomerCommand.ExecuteNonQuery();
 
-            //    }
-            //    connection.Close();
-            // }
-
-
+                //    }
+                connection.Close();
         }
+
+
+    }
     }
 }
